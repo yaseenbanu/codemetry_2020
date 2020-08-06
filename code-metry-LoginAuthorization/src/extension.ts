97095cf9d5cd39d5f7d445bd13b8e362a61fbb72
Shereen789 <irfanashereen@msitprogram.net>
2020-08-06 12:00:30 +0530

    Version 5.0.0

--- code-metry-LoginAuthorization/src/extension.ts
+++ code-metry-LoginAuthorization/src/extension.ts
@@ -1,27 +1,26 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 import { KpmManager } from "./lib/managers/KpmManager";
 import { createCommands } from "./lib/command-helper";
-import { handleSave, createGitToken } from './lib/Loggers';
-import { Socket } from './WebSockets/socket';
-import { CircularBuffer } from './lib/Circular_Buffer/circularbuffer';
-import { resolve } from 'path';
-import { rejects } from 'assert';
+import { createGitToken } from "./lib/Loggers";
+import { Socket } from "./WebSockets/socket";
+import { CircularBuffer } from "./lib/Circular_Buffer/circularbuffer";
+import { resolve } from "path";
+import { rejects } from "assert";
 
 const kpmController: KpmManager = KpmManager.getInstance();
 export let socket = Socket.getInstance();
 export let circularBuffer = CircularBuffer.getInstance();
 
 export function activate(context: vscode.ExtensionContext) {
-
-  vscode.window.showInformationMessage('Hello World from code-metry!');
+  vscode.window.showInformationMessage("Hello World from code-metry!");
   context.subscriptions.push(createCommands(kpmController));
-  vscode.workspace.onDidSaveTextDocument(handleSave);
+  // vscode.workspace.onDidSaveTextDocument(handleSave);
   createGitToken();
-};
-	
-export async function deactivate() { 
+}
+
+export async function deactivate() {
   return new Promise(async (resolve) => {
     (await socket).sendData(circularBuffer);
     (await socket).ws.close();
